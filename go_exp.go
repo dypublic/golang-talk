dc7a7c1445dc03c2c610d7868249114fcf7dc692
dypublic <dypublic@gmail.com>
2018-04-23 00:02:56 +0800

    change

--- go_exp.go
+++ go_exp.go
@@ -28,7 +28,9 @@ func mapNil() {
 /*
 Unexpected Values in Slice and Array "range" Clauses
 level: beginner
-This can happen if you are used to the "for-in" or "foreach" statements in other languages. The "range" clause in Go is different. It generates two values: the first value is the item index while the second value is the item data.
+This can happen if you are used to the "for-in" or "foreach" statements in other languages. 
+The "range" clause in Go is different. 
+It generates two values: the first value is the item index while the second value is the item data.
 */
 //Bad:
 
@@ -50,12 +52,13 @@ func main2() {
 }
 
 /*
-App Exits With Active Goroutines
-level: beginner
-The app will not wait for all your goroutines to complete. This is a common mistake for beginners in general. Everybody starts somewhere, so there's no shame in making rookie mistakes :-)
+Goroutines pattern
 
-
-One of the most common solutions is to use a "WaitGroup" variable. It will allow the main goroutine to wait until all worker goroutines are done. If your app has long running workers with message processing loops you'll also need a way to signal those goroutines that it's time to exit. You can send a "kill" message to each worker. Another option is to close a channel all workers are receiving from. It's a simple way to signal all goroutines at once.
+One of the most common solutions is to use a "WaitGroup" variable. 
+It will allow the main goroutine to wait until all worker goroutines are done. 
+If your app has long running workers with message processing loops you'll also need a way to signal those goroutines that it's time to exit. 
+You can send a "kill" message to each worker. 
+Another option is to close a channel all workers are receiving from. It's a simple way to signal all goroutines at once.
 */
 
 package main
@@ -100,9 +103,12 @@ func doit(workerId int, wq <-chan interface{},done <-chan struct{},wg *sync.Wait
 }
 
 /*Sending to an Closed Channel Causes a Panic
-Receiving from a closed channel is safe. The ok return value in a receive statement will be set to false indicating that no data was received. If you are receiving from a buffered channel you'll get the buffered data first and once it's empty the ok return value will be false.
+Receiving from a closed channel is safe. 
+The ok return value in a receive statement will be set to false indicating that no data was received. 
+If you are receiving from a buffered channel you'll get the buffered data first and once it's empty the ok return value will be false.
 
-Sending data to a closed channel causes a panic. It is a documented behavior, but it's not very intuitive for new Go developers who might expect the send behavior to be similar to the receive behavior.
+Sending data to a closed channel causes a panic. 
+It is a documented behavior, but it's not very intuitive for new Go developers who might expect the send behavior to be similar to the receive behavior.
 */
 package main
 
@@ -159,7 +165,9 @@ func main() {
 /*
 Methods with Value Receivers Can't Change the Original Value
 
-Method receivers are like regular function arguments. If it's declared to be a value then your function/method gets a copy of your receiver argument. This means making changes to the receiver will not affect the original value unless your receiver is a map or slice variable and you are updating the items in the collection or the fields you are updating in the receiver are pointers.
+Method receivers are like regular function arguments. 
+If it's declared to be a value then your function/method gets a copy of your receiver argument. 
+This means making changes to the receiver will not affect the original value unless your receiver is a map or slice variable and you are updating the items in the collection or the fields you are updating in the receiver are pointers.
 */
 package main
 
@@ -199,7 +207,8 @@ func main() {
 /*
 Slice Data "Corruption"
 level: intermediate
-Let's say you need to rewrite a path (stored in a slice). You reslice the path to reference each directory modifying the first folder name and then you combine the names to create a new path.
+Let's say you need to rewrite a path (stored in a slice). 
+You reslice the path to reference each directory modifying the first folder name and then you combine the names to create a new path.
 */
 package main
 
@@ -258,7 +267,8 @@ func main() {
 /*
 Breaking Out of "for switch" and "for select" Code Blocks
 level: intermediate
-A "break" statement without a label only gets you out of the inner switch/select block. If using a "return" statement is not an option then defining a label for the outer loop is the next best thing.
+A "break" statement without a label only gets you out of the inner switch/select block. 
+If using a "return" statement is not an option then defining a label for the outer loop is the next best thing.
 */
 package main
 
@@ -283,8 +293,8 @@ Iteration Variables and Closures in "for" Statements
 This is the most common gotcha in Go.
 The iteration variables in for statements are reused in each iteration.
 This means that each closure (aka function literal) created in your for loop will reference the same variable (and they'll get that variable's value at the time those goroutines start executing).
-*/
-/*Incorrect:
+
+Incorrect:
 */
 package main
 
@@ -402,4 +412,36 @@ func main() {
 	} else {
 		fmt.Println("bad result (res is nil)") //here as expected
 	}
+}
+
+/*
+Time out, move on
+*/
+
+select {
+case msg := <-ch:
+    // a read from ch has occurred
+	process(msg)
+case <-time.After(time.Second):
+    // the read from ch has timed out
+}
+
+/*Let's look at another variation of this pattern. 
+In this example we have a program that reads from multiple replicated databases simultaneously. 
+The program needs only one of the answers, and it should accept the answer that arrives first.
+
+The function Query takes a slice of database connections and a query string. 
+It queries each of the databases in parallel and returns the first response it receives:
+*/
+func Query(conns []Conn, query string) Result {
+    ch := make(chan Result)
+    for _, conn := range conns {
+        go func(c Conn) {
+            select {
+            case ch <- c.DoQuery(query):
+            default:
+            }
+        }(conn)
+    }
+    return <-ch
 }
